7924f3f00aed33580870dda57bfdfbfc1f16c7af
jonnu <jontce@gmail.com>
2012-06-27 15:26:57 +0100

    RPG_Loader will now add html/module path to the view cascade automatically

--- src/core/RPG_Loader.php
+++ src/core/RPG_Loader.php
@@ -5,4 +5,17 @@
 require APPPATH . 'extensions/HMVC/Loader.php';
 
 class RPG_Loader extends HMVC_Loader {
+
+	public function __construct() {
+
+		parent::__construct();
+
+		// Allow module views to exist in VIEWPATH
+		$r_view = realpath(FCPATH . VIEWPATH);
+		$module = $this->_ci_get_component('router')->fetch_module();
+		$r_path = sprintf('%s/%s/', $r_view, $module);
+
+		// Append this path to allowed view paths
+		$this->_ci_view_paths[$r_path] = true;
+	}
 }
