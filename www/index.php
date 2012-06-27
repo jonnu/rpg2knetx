d70246a5b46e622800f442434debfb65d9080032
jonnu <jontce@gmail.com>
2012-06-27 15:25:31 +0100

    Switching back to a raw CodeIgniter index.php file until pathing problems are solved

--- www/index.php
+++ www/index.php
@@ -24,84 +24,188 @@ switch (ENVIRONMENT) {
 }
 
 
-// Paths to system folders
-$system_path        = '/work/2k/common/codeigniter.git/system';
-$application_folder = '/work/2k/git/src';
-$view_folder        = '/work/2k/git/www/html';
+
+/*
+ *---------------------------------------------------------------
+ * SYSTEM FOLDER NAME
+ *---------------------------------------------------------------
+ *
+ * This variable must contain the name of your "system" folder.
+ * Include the path if the folder is not in the same  directory
+ * as this file.
+ */
+	$system_path = '/var/www/common/codeigniter.git/system';
+
+/*
+ *---------------------------------------------------------------
+ * APPLICATION FOLDER NAME
+ *---------------------------------------------------------------
+ *
+ * If you want this front controller to use a different "application"
+ * folder then the default one you can set its name here. The folder
+ * can also be renamed or relocated anywhere on your server. If
+ * you do, use a full server path. For more info please see the user guide:
+ * http://codeigniter.com/user_guide/general/managing_apps.html
+ *
+ * NO TRAILING SLASH!
+ */
+	$application_folder = '../src';
+
+/*
+ *---------------------------------------------------------------
+ * VIEW FOLDER NAME
+ *---------------------------------------------------------------
+ *
+ * If you want to move the view folder out of the application
+ * folder set the path to the folder here. The folder can be renamed
+ * and relocated anywhere on your server. If blank, it will default
+ * to the standard location inside your application folder. If you
+ * do move this, use the full server path to this folder.
+ *
+ * NO TRAILING SLASH!
+ */
+	$view_folder = '../www/html';
 
 
-// Set the current directory correctly for CLI requests
-if (defined('STDIN')) {
-	chdir(dirname(__FILE__));
-}
+/*
+ * --------------------------------------------------------------------
+ * DEFAULT CONTROLLER
+ * --------------------------------------------------------------------
+ *
+ * Normally you will set your default controller in the routes.php file.
+ * You can, however, force a custom routing by hard-coding a
+ * specific controller class/function here. For most applications, you
+ * WILL NOT set your routing here, but it's an option for those
+ * special instances where you might want to override the standard
+ * routing in a specific front controller that shares a common CI installation.
+ *
+ * IMPORTANT: If you set the routing here, NO OTHER controller will be
+ * callable. In essence, this preference limits your application to ONE
+ * specific controller. Leave the function name blank if you need
+ * to call functions dynamically via the URI.
+ *
+ * Un-comment the $routing array below to use this feature
+ */
+	// The directory name, relative to the "controllers" folder.  Leave blank
+	// if your controller is not in a sub-folder within the "controllers" folder
+	// $routing['directory'] = '';
+
+	// The controller class file name.  Example:  Mycontroller
+	// $routing['controller'] = '';
+
+	// The controller function you wish to be called.
+	// $routing['function']	= '';
 
 
-// Check system folder
-$system_path = (($spath = realpath($system_path)) !== false) ? $spath . '/' : rtrim($system_path, '/') . '/';
-if (!is_dir($system_path)) {
-	header('HTTP/1.1 503 Service Unavailable.', true, 503);
-	exit('Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME));	
-}
+/*
+ * -------------------------------------------------------------------
+ *  CUSTOM CONFIG VALUES
+ * -------------------------------------------------------------------
+ *
+ * The $assign_to_config array below will be passed dynamically to the
+ * config class when initialized. This allows you to set custom config
+ * items or override any default config values found in the config.php file.
+ * This can be handy as it permits you to share one application between
+ * multiple front controller files, with each file containing different
+ * config values.
+ *
+ * Un-comment the $assign_to_config array below to use this feature
+ */
+	// $assign_to_config['name_of_config_item'] = 'value of config item';
 
 
-// The name of THIS file
-define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
 
-// Path to the system folder
-define('BASEPATH', str_replace('\\', '/', $system_path));
+// --------------------------------------------------------------------
+// END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
+// --------------------------------------------------------------------
 
-// Path to the front controller (this file)
-define('FCPATH', str_replace(SELF, '', __FILE__));
+/*
+ * ---------------------------------------------------------------
+ *  Resolve the system path for increased reliability
+ * ---------------------------------------------------------------
+ */
 
-// Name of the "system folder"
-define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
-
-
-// The path to the "application" folder
-if (is_dir($application_folder)) {
-
-	if (($_temp = realpath($application_folder)) !== false) {
-		$application_folder = $_temp;
+	// Set the current directory correctly for CLI requests
+	if (defined('STDIN'))
+	{
+		chdir(dirname(__FILE__));
 	}
 
-	define('APPPATH', $application_folder . '/');
-}
-else {
-
-	if (!is_dir(BASEPATH . $application_folder . '/')) {
-		header('HTTP/1.1 503 Service Unavailable.', true, 503);
-		exit('Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF);
+	if (realpath($system_path) !== FALSE)
+	{
+		$system_path = realpath($system_path).'/';
 	}
 
-	define('APPPATH', BASEPATH . $application_folder . '/');
-}
+	// ensure there's a trailing slash
+	$system_path = rtrim($system_path, '/').'/';
 
-
-// The path to the "views" folder
-if (!is_dir($view_folder)) {
-
-	if (!empty($view_folder) && is_dir(APPPATH . $view_folder . '/')) {
-		$view_folder = APPPATH . $view_folder;
+	// Is the system path correct?
+	if ( ! is_dir($system_path))
+	{
+		exit('Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME));
 	}
-	elseif (!is_dir(APPPATH . 'views/')) {
-		header('HTTP/1.1 503 Service Unavailable.', true, 503);
-		exit('Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF);
+
+/*
+ * -------------------------------------------------------------------
+ *  Now that we know the path, set the main path constants
+ * -------------------------------------------------------------------
+ */
+	// The name of THIS file
+	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
+
+	// The PHP file extension
+	// this global constant is deprecated.
+	define('EXT', '.php');
+
+	// Path to the system folder
+	define('BASEPATH', str_replace('\\', '/', $system_path));
+
+	// Path to the front controller (this file)
+	define('FCPATH', str_replace(SELF, '', __FILE__));
+
+	// Name of the "system folder"
+	define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
+
+	// The path to the "application" folder
+	if (is_dir($application_folder))
+	{
+		define('APPPATH', $application_folder.'/');
 	}
-	else {
-		$view_folder = APPPATH . 'views';
+	else
+	{
+		if ( ! is_dir(BASEPATH.$application_folder.'/'))
+		{
+			header('HTTP/1.1 503 Service Unavailable.', TRUE, '503');
+			exit('Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF);
+		}
+
+		define('APPPATH', BASEPATH.$application_folder.'/');
 	}
-}
 
-if (($_temp = realpath($view_folder)) !== false) {
-	$view_folder = realpath($view_folder).'/';
-}
-else {
-	$view_folder = rtrim($view_folder, '/').'/';
-}
+	// The path to the "views" folder
+	if (is_dir($view_folder))
+	{
+		define ('VIEWPATH', $view_folder .'/');
+	}
+	else
+	{
+		if ( ! is_dir(APPPATH.'views/'))
+		{
+			header('HTTP/1.1 503 Service Unavailable.', TRUE, '503');
+			exit('Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF);
+		}
 
-define('VIEWPATH', $view_folder);
+		define ('VIEWPATH', APPPATH.'views/' );
+	}
 
+/*
+ * --------------------------------------------------------------------
+ * LOAD THE BOOTSTRAP FILE
+ * --------------------------------------------------------------------
+ *
+ * And away we go...
+ */
+require_once BASEPATH.'core/CodeIgniter.php';
 
-
-// Bootstrap
-require_once BASEPATH . 'core/CodeIgniter.php';
+/* End of file index.php */
+/* Location: ./index.php */
